206c893243213326d562ca26431d880e6ee89c82
RobertoFavaro <robyfavaro04@gmail.com>
2023-11-29 11:56:41 +0100

    aggiunto un main e una classe Studente

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Main {
+    public static void main(String[] args){
+        Studente studente1 = new Studente("Roberto", "Favaro", "ida376gns");
+
+        System.out.println(studente1);
+    }
+
+}
