398fcd3a4aa9dca0507966eecd2fc9ef00945c32
RobertoFavaro <robyfavaro04@gmail.com>
2023-12-19 00:06:26 +0100

    creato ed eseguito l'esercizio hashset3

--- hashset3/Main.java
+++ hashset3/Main.java
@@ -1,10 +1,11 @@
-package hashset2;
+package hashset3;
 
 import java.util.HashSet;
 
-//Scrivere una funzione che restituisca un HashSet riempito
-//Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
-//Verificare che l' elemento sia parte del Set e stampare il risultato
+//Scrivere una funzione che restituisca un hashset riempito
+//Creare un oggeto dello stesso tipo inserito nell'HashSet e popolarlo
+//Scorrere il set, per ogni elemento verificare se è uguale all'oggetto creato ed eliminarlo
+//Svuotare l'hashset, verificarlo e stampare il risultato
 public class Main {
     public static void main(String[] args) {
         HashSet<String> hashSet = hashSetRiempito();
@@ -23,5 +24,16 @@ public class Main {
         hashSet.add(oggettoUguale);
         boolean esiste = hashSet.contains(oggettoUguale);
         System.out.println("l'elemento " + oggettoUguale + " è dentro l'hashset? " + esiste);
+
+        for(String i: hashSet){
+            if(i == oggettoUguale){
+                hashSet.remove(oggettoUguale);
+            }
+        }
+        System.out.println(hashSet);
+
+        hashSet.clear();
+
+        System.out.println("L'hashset è vuoto? "+ hashSet.isEmpty());
     }
 }
