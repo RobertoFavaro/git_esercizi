d5354a5b6602dee60c92910057dd50e76c018dee
RobertoFavaro <robyfavaro04@gmail.com>
2024-01-19 12:35:06 +0100

    creato ed eseguito l'esercizio Designpatterns_Builder

---
+++ Designpatterns_Builder/Builder.java
@@ -0,0 +1,29 @@
+package Designpatterns_Builder;
+
+public class Builder {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String address;
+    public Builder() {
+    }
+    public Builder setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+    public Builder setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+    public Builder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+    public Builder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+    public Person build() {
+        return new Person(firstName,lastName,age,address);
+    }
+}
